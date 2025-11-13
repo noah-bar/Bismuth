0973c35073cd26d81554a515777c5594d22e6bf6
noah-bar <noah.barberini@gmail.com>
2025-11-13 20:40:03 +0100

    chore(backend): add database and startup configuration

---
+++ database/migrations/1762975659430_create_companies_table.ts
@@ -0,0 +1,21 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'companies'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name')
+      table.string('address')
+      table.string('postal_code')
+      table.string('city')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ start/routes.ts
@@ -0,0 +1,27 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
+const CompaniesController = () => import('#controllers/companies_controller')
+const SessionController = () => import('#controllers/session_controller')
+
+router
+  .group(() => {
+    router.get('/login', [SessionController, 'create']).as('session.create')
+    router.post('/login', [SessionController, 'store']).as('session.store')
+  })
+  .use(middleware.guest())
+
+router
+  .group(() => {
+    router.resource('companies', CompaniesController)
+    router.delete('/logout', [SessionController, 'destroy']).as('session.destroy')
+  })
+  .use(middleware.auth())
