d4391dc7e7ad40a8ccd0239eb1d1a5eeeeb1c1da
noah-bar <noah.barberini@gmail.com>
2025-11-13 20:39:40 +0100

    chore(config): add project configuration files

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import { configApp } from '@adonisjs/eslint-config'
+
+export default [
+  ...configApp(),
+  {
+    files: ['inertia/**/*'],
+    rules: {
+      '@unicorn/filename-case': [
+        'error',
+        {
+          case: 'kebabCase'
+        }
+      ]
+    }
+  }
+]
