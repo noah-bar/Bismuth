72452fbf79977c397bf7af9aec2cf000e74217ac
noah-bar <noah.barberini@gmail.com>
2025-12-07 11:36:33 +0100

    feat(quote): update show page layout with tabs and actions

--- inertia/pages/quotes/show.tsx
+++ inertia/pages/quotes/show.tsx
@@ -1,20 +1,31 @@
-import { QuoteMenubar, QuoteStatusBadge } from '~/features/quote'
+import { QuoteMenubar, QuoteStatusBadge, QuoteTabs } from '~/features/quote'
 import { ReactNode } from 'react'
 import { AppLayout } from '~/components/layouts/app-layout'
 import type { Quote } from '~/types/quote'
 import { Box } from '~/components/shared/box'
+import { Button } from '~/components/ui/button'
+import { ArrowRightIcon, DownloadIcon } from 'lucide-react'
 
 type ShowProps = {
   quote: Quote
 }
 function Show({ quote }: ShowProps) {
   return (
-    <Box className={"h-full"}>
-      <div className={"flex justify-between items-center"}>
+    <div className={'h-full flex flex-col gap-2'}>
+      <Box className={"flex justify-between items-center"}>
         <h1 className={'text-xl font-semibold'}>{quote.title}</h1>
-        <QuoteStatusBadge status={quote.status}/>
-      </div>
-    </Box>
+        <QuoteStatusBadge status={quote.status} />
+      </Box>
+      <QuoteTabs className={'flex-1'} />
+      <Box className={'flex justify-between items-center'}>
+        <Button size={'icon'}>
+          <DownloadIcon />
+        </Button>
+        <Button size={'icon'} variant={'outline'}>
+          <ArrowRightIcon />
+        </Button>
+      </Box>
+    </div>
   )
 }
 Show.layout = (page: ReactNode) => <AppLayout header={<QuoteMenubar />}>{page}</AppLayout>
