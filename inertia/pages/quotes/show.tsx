116e21aebee0ee759b8c1499879ee574d01265fb
noah-bar <noah.barberini@gmail.com>
2025-12-07 11:16:59 +0100

    feat(quote): add title and status badge to quote details page

--- inertia/pages/quotes/show.tsx
+++ inertia/pages/quotes/show.tsx
@@ -1,16 +1,20 @@
-import { QuoteMenubar } from '~/features/quote'
+import { QuoteMenubar, QuoteStatusBadge } from '~/features/quote'
 import { ReactNode } from 'react'
 import { AppLayout } from '~/components/layouts/app-layout'
 import type { Quote } from '~/types/quote'
+import { Box } from '~/components/shared/box'
 
 type ShowProps = {
   quote: Quote
 }
 function Show({ quote }: ShowProps) {
   return (
-    <div>
-
-    </div>
+    <Box className={"h-full"}>
+      <div className={"flex justify-between items-center"}>
+        <h1 className={'text-xl font-semibold'}>{quote.title}</h1>
+        <QuoteStatusBadge status={quote.status}/>
+      </div>
+    </Box>
   )
 }
 Show.layout = (page: ReactNode) => <AppLayout header={<QuoteMenubar />}>{page}</AppLayout>
