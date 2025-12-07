b99b13ffbd0c2ac880047e03ef9c4b432c19ba57
noah-bar <noah.barberini@gmail.com>
2025-12-07 13:01:56 +0100

    feat(quote): add edit action to quote menubar

--- inertia/features/quote/components/quote-menubar.tsx
+++ inertia/features/quote/components/quote-menubar.tsx
@@ -6,11 +6,14 @@ import {
   MenubarTrigger,
 } from '@/components/ui/menubar'
 import { useI18n } from '@/hooks/use-i18n'
-import { Link } from '@inertiajs/react'
-import { CirclePlusIcon } from 'lucide-react'
+import { Link, usePage } from '@inertiajs/react'
+import { CirclePlusIcon, EditIcon } from 'lucide-react'
+import { Quote } from '~/types/quote'
 
 export function QuoteMenubar() {
   const { t } = useI18n()
+  const { quote } = usePage<{ quote?: Quote }>().props
+
   return (
     <Menubar>
       <MenubarMenu>
@@ -22,6 +25,14 @@ export function QuoteMenubar() {
               {t('features.quote.quote-menubar.file.items.new')}
             </Link>
           </MenubarItem>
+          {quote && (
+            <MenubarItem asChild>
+              <Link href={`/quotes/${quote.id}/edit`}>
+                <EditIcon className={'size-4'} />{' '}
+                {t('features.quote.quote-menubar.file.items.edit')}
+              </Link>
+            </MenubarItem>
+          )}
         </MenubarContent>
       </MenubarMenu>
     </Menubar>
