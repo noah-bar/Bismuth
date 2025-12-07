d77586e9d7277a1e87b7e765366a83c619fa693e
noah-bar <noah.barberini@gmail.com>
2025-12-07 10:57:16 +0100

    feat(ui): add url prop to BackButton component

--- inertia/components/shared/back-button.tsx
+++ inertia/components/shared/back-button.tsx
@@ -1,15 +1,21 @@
 import { Button } from '@/components/ui/button'
 import { useConfirm } from '@/hooks/use-confirm'
 import { Undo2Icon } from 'lucide-react'
+import { router } from '@inertiajs/react'
 
 type BackButtonProps = {
   isDirty?: boolean
+  url?: string
 }
-export function BackButton({ isDirty = false }: BackButtonProps) {
+export function BackButton({ isDirty = false, url }: BackButtonProps) {
   const { confirm } = useConfirm()
 
   const handleClick = async () => {
     if (!isDirty || (await confirm())) {
+      if (url) {
+        router.visit(url)
+        return
+      }
       window.history.back()
     }
   }
