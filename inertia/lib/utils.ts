f89bd3117b930a06617c5fa8a04966cee0a900fd
noah-bar <noah.barberini@gmail.com>
2025-12-07 12:51:15 +0100

    feat(utils): add downloadPdf helper function

--- inertia/lib/utils.ts
+++ inertia/lib/utils.ts
@@ -13,3 +13,23 @@ export function formatDate(date: string | Date, locale: string = 'fr-CH'): strin
     year: 'numeric',
   }).format(dateObj)
 }
+
+export async function downloadPdf(url: string, title: string) {
+  const response = await fetch(url, {
+    headers: {
+      Accept: 'application/pdf',
+    },
+  })
+
+  if (response.ok) {
+    const blob = await response.blob()
+    const uri = window.URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = uri
+    a.download = `${title}.pdf`
+    document.body.appendChild(a)
+    a.click()
+    window.URL.revokeObjectURL(uri)
+    document.body.removeChild(a)
+  }
+}
