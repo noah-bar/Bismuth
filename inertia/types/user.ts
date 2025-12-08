22f492710793228decd07d9d9b69486611e24e6f
noah-bar <noah.barberini@gmail.com>
2025-12-08 11:39:10 +0100

    feat(types): add profile fields and update profile type

--- inertia/types/user.ts
+++ inertia/types/user.ts
@@ -5,6 +5,25 @@ export type User = {
   firstName: string
   lastName: string
   email: string
+  address: string
+  city: string
+  postalCode: string
+  phoneNumber: string
+  companyName: string
+  signature: string
+  signatureUrl: string
+  companyIcon: string
+  companyIconUrl: string
   createdAt: DateTime
   updatedAt: DateTime
 }
+
+export type UpdateProfile = Partial<
+  Omit<
+    User,
+    'signature' | 'signatureUrl' | 'companyIcon' | 'companyIconUrl' | 'createdAt' | 'updatedAt'
+  > & {
+    signature?: File
+    companyIcon?: File
+  }
+>
