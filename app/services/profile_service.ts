6b75e8dbacd1c892091c3fea2161aad92e0730cd
noah-bar <noah.barberini@gmail.com>
2025-12-29 13:25:39 +0100

    refactor(services): update ProfileService to use ImageService

--- app/services/profile_service.ts
+++ app/services/profile_service.ts
@@ -3,10 +3,16 @@ import { updateProfileValidator } from '#validators/profile_validator'
 import User from '#models/user'
 import { inject } from '@adonisjs/core'
 import { UploadService } from '#services/upload_service'
+import { ImageService } from '#services/image_service'
+import { MultipartFile } from '@adonisjs/core/bodyparser'
+import { ResizeOptions } from 'sharp'
 
 @inject()
 export class ProfileService {
-  constructor(private uploadService: UploadService) {}
+  constructor(
+    private uploadService: UploadService,
+    private imageService: ImageService
+  ) {}
 
   public async updateProfile(
     profileOrId: number | User,
@@ -17,25 +23,28 @@ export class ProfileService {
       typeof profileOrId === 'number' ? await User.findOrFail(profileOrId) : profileOrId
 
     if (signature) {
-      profile.signature = await this.uploadService.replaceImage(profile.signature, signature, {
+      profile.signature = await this.processAndUploadImage(signature, profile.signature, {
         width: 100,
-        fit: 'cover',
-        quality: 90,
       })
     }
 
     if (companyIcon) {
-      profile.companyIcon = await this.uploadService.replaceImage(
-        profile.companyIcon,
-        companyIcon,
-        {
-          height: 60,
-          fit: 'cover',
-          quality: 90,
-        }
-      )
+      profile.companyIcon = await this.processAndUploadImage(companyIcon, profile.companyIcon, {
+        height: 100,
+      })
     }
 
     return await profile.merge(rest).save()
+  }
+
+  private async processAndUploadImage(
+    file: MultipartFile,
+    existingPath: string,
+    options: ResizeOptions
+  ): Promise<string> {
+    const buffer = await this.uploadService.toBuffer(file)
+    const resizedImage = await this.imageService.resize(buffer, options)
+    const webpImage = await this.imageService.toWebp(resizedImage)
+    return await this.uploadService.replaceBuffer(existingPath, webpImage, 'webp')
   }
 }
