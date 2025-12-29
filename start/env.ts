ef87458b8ab9a3f90b1859816c110a5239858456
noah-bar <noah.barberini@gmail.com>
2025-12-29 19:57:16 +0100

    feat(database): migrate from sqlite to mysql

--- start/env.ts
+++ start/env.ts
@@ -31,4 +31,15 @@ export default await Env.create(new URL('../', import.meta.url), {
   |----------------------------------------------------------
   */
   DRIVE_DISK: Env.schema.enum(['fs'] as const),
+
+  /*
+  |----------------------------------------------------------
+  | Variables for configuring database connection
+  |----------------------------------------------------------
+  */
+  DB_HOST: Env.schema.string({ format: 'host' }),
+  DB_PORT: Env.schema.number(),
+  DB_USER: Env.schema.string(),
+  DB_PASSWORD: Env.schema.string.optional(),
+  DB_DATABASE: Env.schema.string()
 })
