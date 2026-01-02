e2ee1b359dfdaa0c048c97c61a9f018e729013d3
noah-bar <noah.barberini@gmail.com>
2026-01-02 23:20:45 +0100

    feat(login): add background and rounded styling to login form

--- inertia/pages/login/create.tsx
+++ inertia/pages/login/create.tsx
@@ -5,7 +5,7 @@ export default function Create() {
   return (
     <GuessLayout>
       <div className={'size-full flex justify-center items-center p-4 overflow-hidden'}>
-        <LoginForm className={'w-full max-w-md'} />
+        <LoginForm className={'w-full max-w-md bg-background p-2 rounded-lg'} />
       </div>
     </GuessLayout>
   )
