2811ca638d68d3931c2edfe495ffa8cadd34a487
noah-bar <noah.barberini@gmail.com>
2026-01-02 23:20:21 +0100

    feat(ui): add header with logo, version and theme toggle to guest layout

--- inertia/components/layouts/guess-layout.tsx
+++ inertia/components/layouts/guess-layout.tsx
@@ -1,6 +1,9 @@
 import { ReactNode } from 'react'
 import { RootLayout } from '~/components/layouts/root-layout'
 import { SafeArea } from '~/components/ui/safe-area'
+import { ThemeToggle } from '~/components/shared/theme-toggle'
+import { AppVersion } from '~/components/shared/app-version'
+import { AppLogo } from '~/components/shared/app-logo'
 
 export type GuessLayoutProps = {
   children?: ReactNode
@@ -8,7 +11,18 @@ export type GuessLayoutProps = {
 export function GuessLayout({ children }: GuessLayoutProps) {
   return (
     <RootLayout>
-      <SafeArea className={'h-screen w-full bg-background'}>{children}</SafeArea>
+      <SafeArea className={'flex flex-col h-screen w-full bg-bismuth bg-background'}>
+        <header className={'w-full h-14 bg-background border-border border-b px-4'}>
+          <div className={'flex justify-between h-full container mx-auto'}>
+            <AppLogo className={'w-auto'} />
+            <div className={'flex items-center gap-2'}>
+              <AppVersion />
+              <ThemeToggle />
+            </div>
+          </div>
+        </header>
+        <main className={'flex-1'}>{children}</main>
+      </SafeArea>
     </RootLayout>
   )
 }
